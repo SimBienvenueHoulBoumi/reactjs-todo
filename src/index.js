47019c2e6dace23354373d348c843c092dba4036
xenon-cripton <brhulla@gmail.com>
2023-03-09 00:04:27 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/index.css';
+import Home from './pages/Home';
+import Login from './pages/login';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
